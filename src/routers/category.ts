11f5aa57b7b0dc6f5323e89094e26326118327fa
chonlachit <150209397+chonlachit2233@users.noreply.github.com>
2026-03-09 21:00:29 +0700

    update ui 9/3/2569

--- src/routers/category.ts
+++ src/routers/category.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express'
 const router = express.Router()
 import { CategoryCreate, Listcategory, Removecategory } from '../controllers/category'
+import {Authcheck, Admincheck} from '../middenware/auth-admin-check'
 
-router.post('/category', CategoryCreate)
+router.post('/category', Authcheck, Admincheck,CategoryCreate)
 router.get('/category', Listcategory)
-router.delete('/category/:id', Removecategory)
+router.delete('/category/:id',Authcheck,Admincheck, Removecategory)
 
 export default router
